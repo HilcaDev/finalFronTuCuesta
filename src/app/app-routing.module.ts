ec9e17eade303241aa3724d998f44ee9a51ba46a
AlexBMacLeod <macale@sas.upenn.edu>
2021-11-29 16:27:00 -0300

    Se crea login, registro e inicio - Usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './usuario/inicio/inicio.component';
+import { LoginComponent } from './usuario/login/login.component';
+import { RegistrarComponent } from './usuario/registrar/registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: LoginComponent
+  },
+  {
+    path:"registrarse", // Lo nombro como yo desee
+    component: RegistrarComponent
+  },
+  {
+    path:"inicio", // Lo nombro como yo desee
+    component: InicioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './usuario/login/login.component';
+import { RegistrarComponent } from './usuario/registrar/registrar.component';
+import { InicioComponent } from './usuario/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
